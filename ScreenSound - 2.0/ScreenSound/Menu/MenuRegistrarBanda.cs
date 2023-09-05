be4d2cd42af513d8b4aab5fcfceebe65b7e501b6
Leonardo Vicentin <60295949+LeoVicentin96@users.noreply.github.com>
2023-09-05 17:18:11 -0300

    Add files via upload

---
+++ ScreenSound - 2.0/ScreenSound/Menu/MenuRegistrarBanda.cs
@@ -0,0 +1,31 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
+
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em um parágrafo. Adote um estilo informal");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ ScreenSound - 2.0/ScreenSound/Modelos/IAvaliavel.cs
@@ -0,0 +1,7 @@
+﻿namespace ScreenSound.Modelos;
+
+internal interface IAvaliavel
+{
+    void AdicionarNota(Avaliacao nota);
+    double Media { get; }
+}
